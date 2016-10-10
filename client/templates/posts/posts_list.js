d57e10cc5276b0f787dc5a3f7d82b34e6d2540b2
ttop5 <ttop5@qq.com>
2016-10-10 18:05:54 +0800

    feat: 在posts集合中加入数据并把集合连接到postsList模板上

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,18 +1,5 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'Blog',
-    url: 'https://github.com/ttop5/blog/issues'
-  }
-];
-
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
